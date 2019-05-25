40b70c9dcbc7112c6fe451ccc1074e18b44125c0
siddharth1609 <siddharthsonwane18@gmail.com>
2019-05-25 23:58:23 +0530

    byte  code samople added

---
+++ src/all/ByteSample.java
@@ -0,0 +1,20 @@
+package all;
+
+public class ByteSample {
+
+	public static void main(String[] args) {
+
+		// byte a = "1";// Type mismatch: cannot convert from String to byte
+		// byte a = 1; // 1
+
+		byte a = '1'; // 49 ascii
+
+		System.out.println(":" + a);
+
+		char c = '1';
+
+		System.out.println(":" + c);
+
+	}
+
+}
